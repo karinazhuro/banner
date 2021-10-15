c87518e2dd6eb3c3683b69d8a99d7f45184e1160
Karina Zhuro <kzhuro36@gmail.com>
2021-10-15 12:52:54 +0300

    feat: add files with translate

---
+++ main.js
@@ -0,0 +1,27 @@
+import en from "./localization/en.json";
+// import es from "./localization/es.json";
+// import fr from "./localization/fr.json";
+// import ja from "./localization/ja.json";
+// import nl from "./localization/nl.json";
+// import ru from "./localization/ru.json";
+// import zh from "./localization/zh.json";
+
+console.log(en);
+
+// const lang = document.getElementsByTagName("html")[0]
+// 	.getAttribute("lang");
+
+// const languageCodes = ["en", "es", "fr", "ja", "nl", "ru", "zh"];
+
+// const languages = {
+// 	en,
+// 	es,
+// 	fr,
+// 	ja,
+// 	nl,
+// 	ru,
+// 	zh,
+// };
+
+// const restore = document.getElementById('restore');
+// restore.innerText = languages[lang.Restore]
